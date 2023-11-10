95814a45ac77e157ffd3c22f9fe3660a375491cc
hongkikii <hyangmeeh@icloud.com>
2023-11-11 01:27:45 +0900

    feat: 방문 날짜 및 주문 입력 기능 구현

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,136 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.Menu;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputView {
+    private int date;
+    private String order;
+
+    public InputView() {
+        this.date = 0;
+        this.order = "";
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void introduce() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public int readDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String date;
+        while (true) {
+            date = Console.readLine();
+            try {
+                validateDate(date);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return Integer.parseInt(date);
+    }
+
+    private void validateDate(String date) {
+        checkNumber(date);
+        checkBoundary(date);
+    }
+
+    private void checkNumber(String date) {
+        try {
+            Integer.parseInt(date);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void checkBoundary(String date) {
+        int number = Integer.parseInt(date);
+        if (number < 1 || number > 31) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public String readOrder() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String order;
+        while (true) {
+            order = Console.readLine();
+            try {
+                validateOrder(order);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return order;
+    }
+
+    private void validateOrder(String order) {
+        checkForm(order);
+        String[] orders = order.split(",");
+        checkNoMenu(orders);
+        checkCount(orders); // 1이상, 전체 20이하
+        checkDuplicateMenu(orders);
+    }
+
+    private void checkForm(String order) {
+        String regex = "^[가-힣]+-\\d+(,[가-힣]+-\\d+)*$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(order);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.1");
+        }
+    }
+
+    private void checkNoMenu(String[] orders) {
+        for (String order : orders) {
+            String name = order.split("-")[0];
+            if (contain(name)) {
+                return;
+            }
+        }
+        throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.2");
+    }
+
+    private boolean contain(String name) {
+        for (Menu menu : Menu.values()) {
+            if (menu.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void checkCount(String[] orders) {
+        for (String order : orders) {
+            String count = order.split("-")[1];
+            int number = Integer.parseInt(count);
+            if (number < 1 || number > 20) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.3");
+            }
+        }
+    }
+
+    private void checkDuplicateMenu(String[] orders) {
+        HashSet<String> noDuplicateMenu = new HashSet<>();
+        for (String order : orders) {
+            String name = order.split("-")[0];
+            noDuplicateMenu.add(name);
+        }
+        if (noDuplicateMenu.size() != orders.length) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.4");
+        }
+    }
+}
