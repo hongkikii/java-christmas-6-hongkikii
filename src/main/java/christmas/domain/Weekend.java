174b40cb49672fb3c6bfd233002feef44cc22964
hongkikii <hyangmeeh@icloud.com>
2023-11-11 02:05:08 +0900

    feat: 주말 할인 구현

---
+++ src/main/java/christmas/domain/Weekend.java
@@ -0,0 +1,29 @@
+package christmas.domain;
+
+public class Weekend extends Discount {
+
+    public Weekend(String name) {
+        super(name);
+    }
+
+    @Override
+    public void calculate(int date, Order order) {
+        if (isWeekend(date)) {
+            int countMain = order.countMain();
+            save(2023 * countMain);
+        }
+    }
+
+    private boolean isWeekend(int date) {
+        if (date == 1 || date == 2 || date == 8 || date == 9) {
+            return true;
+        }
+        if (date == 15 || date == 16 || date == 22 || date == 23) {
+            return true;
+        }
+        if (date == 29 || date == 30) {
+            return true;
+        }
+        return false;
+    }
+}
