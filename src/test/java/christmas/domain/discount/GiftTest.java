291f9fb918c722f2341bbe842f5a6e26a1eabf50
hongkikii <hyangmeeh@icloud.com>
2023-11-15 22:56:33 +0900

    test: 증정 이벤트 테스트 불필요 import 제거

--- src/test/java/christmas/domain/discount/GiftTest.java
+++ src/test/java/christmas/domain/discount/GiftTest.java
@@ -4,7 +4,6 @@ package christmas.domain.discount;
 import static org.assertj.core.api.Assertions.*;
 
 import christmas.domain.Order;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
