c52c1821f1f352167d1d6d5b942d1c18c0f2ec27
hongkikii <hyangmeeh@icloud.com>
2023-11-15 17:34:58 +0900

    refactor: Special 메소드 간소화

--- src/main/java/christmas/domain/discount/Special.java
+++ src/main/java/christmas/domain/discount/Special.java
@@ -4,7 +4,6 @@ import static christmas.constant.Constants.Special.*;
 import static christmas.domain.DayInformation.*;
 import static christmas.domain.DayInformation.SpecialType.*;
 
-import christmas.domain.DayInformation;
 import christmas.domain.Order;
 
 public class Special extends Discount {
@@ -21,10 +20,6 @@ public class Special extends Discount {
     }
 
     private boolean isSpecial(int date) {
-        DayInformation dayInformation = getDayInformation(date);
-        if (dayInformation.getSpecialType() == SPECIAL) {
-            return true;
-        }
-        return false;
+        return getDayInformation(date).getSpecialType() == SPECIAL;
     }
 }
