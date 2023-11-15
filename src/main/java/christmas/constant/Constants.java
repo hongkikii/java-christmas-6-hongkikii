85acb7bcb827361a15444690c44f71bc14cd87ac
hongkikii <hyangmeeh@icloud.com>
2023-11-15 17:58:32 +0900

    style: Constants 필요없는 상수 제거, 상수 타입 변경

--- src/main/java/christmas/constant/Constants.java
+++ src/main/java/christmas/constant/Constants.java
@@ -7,8 +7,8 @@ public class Constants {
         public static final String ORDER_SPLIT = ",";
         public static final String X = "없음";
 
-        public static final Integer ZERO = 0;
-        public static final Integer MAX_ORDER_COUNT = 20;
+        public static final int ZERO = 0;
+        public static final int MAX_ORDER_COUNT = 20;
     }
 
     public class Error {
@@ -28,12 +28,12 @@ public class Constants {
         public static final String DATE = "date";
         public static final String ORDER_REGEX = "^[가-힣]+-\\d+(,[가-힣]+-\\d+)*$";
 
-        public static final Integer DECEMBER_START = 1;
-        public static final Integer DECEMBER_END = 31;
-        public static final Integer MIN_ORDER_COUNT = 1;
+        public static final int DECEMBER_START = 1;
+        public static final int DECEMBER_END = 31;
+        public static final int MIN_ORDER_COUNT = 1;
 
-        public static final Integer MENU_NAME_INDEX = 0;
-        public static final Integer MENU_COUNT_INDEX = 1;
+        public static final int MENU_NAME_INDEX = 0;
+        public static final int MENU_COUNT_INDEX = 1;
     }
 
     public class OutputView {
@@ -58,32 +58,30 @@ public class Constants {
 
     public class DDay {
         public static final String DDAY_DISCOUNT_NAME = "크리스마스 디데이 할인";
-        public static final Integer DDAY = 25;
-        public static final Integer BASE_DISCOUNT = 1000;
-        public static final Integer ADD_DISCOUNT = 100;
-        public static final Integer ONE = 1;
+        public static final int DDAY = 25;
+        public static final int BASE_DISCOUNT = 1000;
+        public static final int ADD_DISCOUNT = 100;
+        public static final int ONE = 1;
     }
 
     public class Gift {
         public static final String GIFT_DISCOUNT_NAME = "증정 이벤트";
-        public static final Integer GIFT_CHAMPAGNE_COUNT = 1;
-        public static final Integer MIN_GIFT_PRICE = 120000;
-        public static final Integer CHAMPAGNE_PRICE = 25000;
+        public static final int MIN_GIFT_PRICE = 120000;
     }
 
     public class Special {
         public static final String SPECIAL_DISCOUNT_NAME = "특별 할인";
-        public static final Integer SPECIAL_BASE_DISCOUNT = 1000;
+        public static final int SPECIAL_BASE_DISCOUNT = 1000;
     }
 
     public class Weekday {
         public static final String WEEKDAY_DISCOUNT_NAME = "평일 할인";
-        public static final Integer WEEKDAY_BASE_DISCOUNT = 2023;
+        public static final int WEEKDAY_BASE_DISCOUNT = 2023;
     }
 
     public class Weekend {
         public static final String WEEKEND_DISCOUNT_NAME = "주말 할인";
-        public static final Integer WEEKEND_BASE_DISCOUNT = 2023;
+        public static final int WEEKEND_BASE_DISCOUNT = 2023;
     }
 
     public class Badge {
@@ -91,43 +89,43 @@ public class Constants {
         public static final String TREE_NAME = "트리";
         public static final String SANTA_NAME = "산타";
 
-        public static final Integer STAR_MIN_PRICE = 5000;
-        public static final Integer TREE_MIN_PRICE = 10000;
-        public static final Integer SANTA_MIN_PRICE = 20000;
+        public static final int STAR_MIN_PRICE = 5000;
+        public static final int TREE_MIN_PRICE = 10000;
+        public static final int SANTA_MIN_PRICE = 20000;
     }
 
     public class DayInformation {
-        public static final Integer DAY_ONE = 1;
-        public static final Integer DAY_TWO = 2;
-        public static final Integer DAY_THREE = 3;
-        public static final Integer DAY_FOUR = 4;
-        public static final Integer DAY_FIVE = 5;
-        public static final Integer DAY_SIX = 6;
-        public static final Integer DAY_SEVEN = 7;
-        public static final Integer DAY_EIGHT = 8;
-        public static final Integer DAY_NINE = 9;
-        public static final Integer DAY_TEN = 10;
-        public static final Integer DAY_ELEVEN = 11;
-        public static final Integer DAY_TWELVE = 12;
-        public static final Integer DAY_THIRTEEN = 13;
-        public static final Integer DAY_FOURTEEN = 14;
-        public static final Integer DAY_FIFTEEN = 15;
-        public static final Integer DAY_SIXTEEN = 16;
-        public static final Integer DAY_SEVENTEEN = 17;
-        public static final Integer DAY_EIGHTEEN = 18;
-        public static final Integer DAY_NINETEEN = 19;
-        public static final Integer DAY_TWENTY = 20;
-        public static final Integer DAY_TWENTY_ONE = 21;
-        public static final Integer DAY_TWENTY_TWO = 22;
-        public static final Integer DAY_TWENTY_THREE = 23;
-        public static final Integer DAY_TWENTY_FOUR = 24;
-        public static final Integer DAY_TWENTY_FIVE = 25;
-        public static final Integer DAY_TWENTY_SIX = 26;
-        public static final Integer DAY_TWENTY_SEVEN = 27;
-        public static final Integer DAY_TWENTY_EIGHT = 28;
-        public static final Integer DAY_TWENTY_NINE = 29;
-        public static final Integer DAY_THIRTY = 30;
-        public static final Integer DAY_THIRTY_ONE = 31;
+        public static final int DAY_ONE = 1;
+        public static final int DAY_TWO = 2;
+        public static final int DAY_THREE = 3;
+        public static final int DAY_FOUR = 4;
+        public static final int DAY_FIVE = 5;
+        public static final int DAY_SIX = 6;
+        public static final int DAY_SEVEN = 7;
+        public static final int DAY_EIGHT = 8;
+        public static final int DAY_NINE = 9;
+        public static final int DAY_TEN = 10;
+        public static final int DAY_ELEVEN = 11;
+        public static final int DAY_TWELVE = 12;
+        public static final int DAY_THIRTEEN = 13;
+        public static final int DAY_FOURTEEN = 14;
+        public static final int DAY_FIFTEEN = 15;
+        public static final int DAY_SIXTEEN = 16;
+        public static final int DAY_SEVENTEEN = 17;
+        public static final int DAY_EIGHTEEN = 18;
+        public static final int DAY_NINETEEN = 19;
+        public static final int DAY_TWENTY = 20;
+        public static final int DAY_TWENTY_ONE = 21;
+        public static final int DAY_TWENTY_TWO = 22;
+        public static final int DAY_TWENTY_THREE = 23;
+        public static final int DAY_TWENTY_FOUR = 24;
+        public static final int DAY_TWENTY_FIVE = 25;
+        public static final int DAY_TWENTY_SIX = 26;
+        public static final int DAY_TWENTY_SEVEN = 27;
+        public static final int DAY_TWENTY_EIGHT = 28;
+        public static final int DAY_TWENTY_NINE = 29;
+        public static final int DAY_THIRTY = 30;
+        public static final int DAY_THIRTY_ONE = 31;
     }
 
     public class Menu {
@@ -143,22 +141,22 @@ public class Constants {
         public static final String ZERO_COLA_NAME = "제로콜라";
         public static final String RED_WINE_NAME = "레드와인";
         public static final String CHAMPAGNE_NAME = "샴페인";
-        public static final Integer MUSHROOM_SOUP_PRICE = 6000;
 
-        public static final Integer TAPAS_PRICE = 5500;
-        public static final Integer CAESAR_SALAD_PRICE = 8000;
-        public static final Integer T_BONE_STAKE_PRICE = 55000;
-        public static final Integer BBQ_RIBS_PRICE = 54000;
-        public static final Integer SEAFOOD_PASTA_PRICE = 35000;
-        public static final Integer CHRISTMAS_PASTA_PRICE = 25000;
-        public static final Integer CHOCOLATE_CAKE_PRICE = 15000;
-        public static final Integer ICE_CREAM_PRICE = 5000;
-        public static final Integer ZERO_COLA_PRICE = 3000;
-        public static final Integer RED_WINE_PRICE = 60000;
-        public static final Integer CHAMPAGNE_PRICE = 25000;
+        public static final int MUSHROOM_SOUP_PRICE = 6000;
+        public static final int TAPAS_PRICE = 5500;
+        public static final int CAESAR_SALAD_PRICE = 8000;
+        public static final int T_BONE_STAKE_PRICE = 55000;
+        public static final int BBQ_RIBS_PRICE = 54000;
+        public static final int SEAFOOD_PASTA_PRICE = 35000;
+        public static final int CHRISTMAS_PASTA_PRICE = 25000;
+        public static final int CHOCOLATE_CAKE_PRICE = 15000;
+        public static final int ICE_CREAM_PRICE = 5000;
+        public static final int ZERO_COLA_PRICE = 3000;
+        public static final int RED_WINE_PRICE = 60000;
+        public static final int CHAMPAGNE_PRICE = 25000;
     }
 
     public class Order {
-        public static final Integer MIN_EVENT_APPLY_PRICE = 10000;
+        public static final int MIN_EVENT_APPLY_PRICE = 10000;
     }
 }
