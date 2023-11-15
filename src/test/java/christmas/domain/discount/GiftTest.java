0a014f6e86f3c9ca12fb0dbea5eb882f23ddbc3a
hongkikii <hyangmeeh@icloud.com>
2023-11-15 22:16:58 +0900

    test: 증정 이벤트 테스트 중복 코드 메소드 분리

--- src/test/java/christmas/domain/discount/GiftTest.java
+++ src/test/java/christmas/domain/discount/GiftTest.java
@@ -1,6 +1,8 @@
 package christmas.domain.discount;
 
 
+import static org.assertj.core.api.Assertions.*;
+
 import christmas.domain.Order;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,29 +24,27 @@ class GiftTest {
     @Test
     void checkWeekend() {
         order.save(giftMenu);
-        Gift gift = applyDiscount(normalDate);
-        Assertions.assertThat(gift.getPrice()).isNotEqualTo(0);
+        Gift gift = new Gift();
+        gift.calculate(normalDate, order);
+        assertThat(gift.getPrice()).isNotEqualTo(0);
     }
 
     @DisplayName("할인 전 총주문 금액이 12만원 미만일 때 증정 이벤트를 적용하지 않는다.")
     @Test
     void checkWeekday() {
-        order.save(noGiftMenu);
-        Gift gift = applyDiscount(normalDate);
-        Assertions.assertThat(gift.getPrice()).isEqualTo(0);
+        expectEqual(noGiftMenu, normalDate, 0);
     }
 
     @DisplayName("샴페인 1개 금액의 할인을 적용한다.")
     @Test
     void checkSameMenuDiscount() {
-        order.save(giftMenu);
-        Gift gift = applyDiscount(normalDate);
-        Assertions.assertThat(gift.getPrice()).isEqualTo(25000);
+        expectEqual(giftMenu, normalDate, 25000);
     }
 
-    Gift applyDiscount(int date) {
-        Gift result = new Gift();
-        result.calculate(date, order);
-        return result;
+    void expectEqual(String menu, int date, int result) {
+        order.save(menu);
+        Gift gift = new Gift();
+        gift.calculate(date, order);
+        assertThat(gift.getPrice()).isEqualTo(result);
     }
 }
